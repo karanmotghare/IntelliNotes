b400d7f1694e1e7f15be6b5a979cf4751d4b78f0
karan <karanmotghare8@gmail.com>
2025-12-11 23:29:28 +0530

    navigation init

---
+++ app/src/main/java/com/example/intellinotes/navigation/AppHostNav.kt
@@ -0,0 +1,12 @@
+package com.example.intellinotes.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppHostNav(
+    navController: NavHostController = rememberNavController()
+) {
+
+}
