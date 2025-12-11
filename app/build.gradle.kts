c84525e6313689667cadc93a79269a833d6975b4
karan <karanmotghare8@gmail.com>
2025-12-11 21:53:42 +0530

    agp downgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.intellinotes"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.intellinotes"
