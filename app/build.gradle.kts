a55fc0e8df5d9ff23d17b46a584d9b2d2e5c1db1
Karan.Motghare <karan.motghare@magicbricks.com>
2025-12-14 11:53:16 +0530

    removing unnecessary dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.navigation.compose.jvmstubs)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
