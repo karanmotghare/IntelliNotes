3d9046c919f696d9610bd7a3f4b9dd6377740862
Karan.Motghare <karan.motghare@magicbricks.com>
2026-02-02 20:30:59 +0530

    adding test cases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,9 +95,13 @@ dependencies {
     // Add the dependency for the Firebase SDK for Google Analytics
     implementation("com.google.firebase:firebase-analytics")
 
-    // TODO: Add the dependencies for any other Firebase products you want to use
     // See https://firebase.google.com/docs/android/setup#available-libraries
     // For example, add the dependencies for Firebase Authentication and Cloud Firestore
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
