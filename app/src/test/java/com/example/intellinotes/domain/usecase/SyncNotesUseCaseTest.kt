013ba672e56206cd98de422bc768f356752b32c9
karan <karanmotghare8@gmail.com>
2026-02-04 12:45:30 +0530

    mockito first test case

---
+++ app/src/test/java/com/example/intellinotes/domain/usecase/SyncNotesUseCaseTest.kt
@@ -0,0 +1,83 @@
+package com.example.intellinotes.domain.usecase
+
+import com.example.intellinotes.data.remote.mapper.toCloud
+import com.example.intellinotes.data.repository.repo.NoteRepository
+import com.example.intellinotes.data.repository.repo.NotesRemoteRepository
+import com.example.intellinotes.data.room.entity.NoteEntity
+import com.example.intellinotes.data.room.entity.NoteType
+import com.example.intellinotes.domain.usecases.SyncNotesUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SyncNotesUseCaseTest {
+
+    // ─────────────────────
+    // Step 1: Create mocks
+    // ─────────────────────
+    private val repository : NoteRepository = mock()
+    private val remoteRepository : NotesRemoteRepository = mock()
+
+    private lateinit var useCase: SyncNotesUseCase
+
+    @Before
+    fun setup() {
+        useCase = SyncNotesUseCase(
+            repository,
+            remoteRepository
+        )
+    }
+
+    // ─────────────────────
+    // Step 2: Fake data
+    // ─────────────────────
+
+    private val note = NoteEntity(
+        id = "1",
+        title = "Test",
+        content = "Content",
+        noteType = NoteType.TEXT,
+        isDeleted = false,
+        createdAt = 1L,
+        updatedAt = 2L,
+        version = 1,
+        isSynced = false,
+        lastSyncedAt = null,
+        isPinned = false,
+        aiSummary = null,
+        aiKeywords = null,
+        aiEmbeddingId = null,
+        coverImageUri = null,
+        attachmentsCount = 0,
+        folderId = "1234"
+    )
+
+    // ─────────────────────
+    // Step 3: Actual tests
+    // ─────────────────────
+    @Test
+    fun `uploads unsynced notes and marked them synced`() = runTest {
+        //given
+        whenever(repository.getUnsyncedNotes()).thenReturn(listOf(note))
+        whenever(repository.getDeletedNotes()).thenReturn(emptyList())
+        whenever(remoteRepository.fetchAllNotes()).thenReturn(emptyList())
+
+        //when
+        useCase()
+
+        //then
+        val expectedCloudNote = note.toCloud()
+        verify(remoteRepository).uploadNote(expectedCloudNote)
+        verify(repository).markAsSynced(eq(note.id), any())
+
+    }
+
+
+}
