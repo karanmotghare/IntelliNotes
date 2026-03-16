e2141f0944ddab71745cbde1e3b604eb962facf4
karan <karanmotghare8@gmail.com>
2026-03-16 22:53:10 +0530

    Internet permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.karan.intellinotes"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
